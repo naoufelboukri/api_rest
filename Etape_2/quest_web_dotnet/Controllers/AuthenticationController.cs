f973b6d5fb502b00e36d53914c4a2d50f41439b0
AbdouRGitHub <abdousani60@gmail.com>
2023-04-16 18:25:31 +0200

    restore controller

--- Etape_2/quest_web_dotnet/Controllers/AuthenticationController.cs
+++ Etape_2/quest_web_dotnet/Controllers/AuthenticationController.cs
@@ -33,7 +33,7 @@ namespace quest_web.Controllers
                 await _context.SaveChangesAsync();
 
 
-                return StatusCode(201, new UserDetails(username, user.Role));
+                return CreatedAtAction(nameof(register), new UserDetails(username, user.Role));
             }
             catch (Exception ex)
             {
