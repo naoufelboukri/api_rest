4e98be3a8c817e8acc53c176bb6548f4b991c8bb
Naoufel <naoufelboukri@gmail.com>
2023-05-02 13:51:17 +0200

    wip login

--- Etape_5/front/src/app/Components/Login/login.component.ts
+++ Etape_5/front/src/app/Components/Login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  isValid: boolean = true;
 
+  public constructor(
+    private _router: Router,
+    private _authService: AuthService
+  ) { }
+
+  login(email: string, password: string) {
+    this._authService.login(email, password).subscribe(
+      (data) => {
+        this.isValid = true;
+        localStorage.setItem('UserToken', data.toString());
+        window.location.reload();
+      },
+      (err) => {
+        this.isValid = false;
+      }
+    )
+  }
+
+  goToRegister() {
+    this._router.navigate(['register']);
+  }
 }
--- Etape_5/front/src/app/app.module.ts
+++ Etape_5/front/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './Components/Register/register.component';
 import { LoginComponent } from './Components/Login/login.component';
+import { NgForm } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,9 +17,13 @@ import { LoginComponent } from './Components/Login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    HttpClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
