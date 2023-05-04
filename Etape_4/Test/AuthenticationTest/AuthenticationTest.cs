54083bcb8adb62e58b694b01a550cbcf70de2791
AbdouRGitHub <abdousani60@gmail.com>
2023-05-04 19:47:09 +0200

    AuthenticationTest Complet

--- Etape_4/Test/AuthenticationTest/AuthenticationTest.cs
+++ Etape_4/Test/AuthenticationTest/AuthenticationTest.cs
@@ -1,12 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Any;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using quest_web;
 using quest_web.Controllers;
+using quest_web.Models;
 using quest_web.Models.Form;
 using quest_web.Utils;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net;
+using System.Text.Json.Nodes;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -120,9 +125,18 @@ namespace Test
             };
 
             _authenticationController.register(user);
-            var token = _authenticationController.authenticate(user);
+            var authenticate = _authenticationController.authenticate(user);
 
+            var obj = JsonConvert.SerializeObject(authenticate);
 
+            JObject jsonToken = JObject.Parse(obj);
+            string authorization = "Bearer " + (string)jsonToken["Result"]["Value"]["token"];
+
+            var result = _authenticationController.me(authorization);
+
+            var response = Assert.IsType<OkObjectResult>(result);
+
+            Assert.IsType<UserDetails>(response.Value);
         }
     }
 }
---
+++ Etape_4/Test/UserTest/UserTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using quest_web;
+using quest_web.Controllers;
+using quest_web.Models.Form;
+using quest_web.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Test
+{
+    public class UserTest
+    {
+        private readonly ITestOutputHelper output;
+        public UserTest(ITestOutputHelper output) {
+            this.output = output;
+        }
+
+        [Fact]
+        public void userWithoutToken()
+        {
+            var _jwt = new JwtTokenUtil();
+            var database = new DbContextOptionsBuilder<APIDbContext>()
+                .UseInMemoryDatabase(databaseName: "quest_web")
+                .Options;
+            var _context = new APIDbContext(database);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+
+            var _userController = new UserController(_context, _jwt);
+            var _authenticationController = new AuthenticationController(_context, _jwt);
+
+            var user = new UserBody
+            {
+                username = "test",
+                password = "test"
+            };
+
+            _authenticationController.register(user);
+            var authenticate = _authenticationController.authenticate(user);
+
+            var obj = JsonConvert.SerializeObject(authenticate);
+
+            JObject jsonToken = JObject.Parse(obj);
+
+            string authorization = "Bearer " + (string)jsonToken["Result"]["Value"]["token"];
+
+            var result = _userController.allUsers(authorization);
+        }
+    }
+
+}
