b1031389b7105608a7adac1026dd186252f036dd
Naoufel <naoufelboukri@gmail.com>
2023-05-04 19:41:18 +0200

    Add CORS allow method and add authorization on method users and user/id

--- Etape_4/quest_web_dotnet/Program.cs
+++ Etape_4/quest_web_dotnet/Program.cs
@@ -42,7 +42,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:4200").AllowAnyHeader();
+                          policy.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
                       });
 });
 
