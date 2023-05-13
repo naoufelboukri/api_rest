ab96f3c3ed22cb5c7784ea0c00676569ffce448f
Naoufel <naoufelboukri@gmail.com>
2023-05-13 17:17:05 +0200

    Add Post Controller

--- Etape_6/quest_web_dotnet/Services/JwtService.cs
+++ Etape_6/quest_web_dotnet/Services/JwtService.cs
@@ -7,10 +7,12 @@ namespace quest_web_dotnet.Services
     public class JwtService
     {
         private readonly JwtTokenUtil _jwt;
+        private readonly APIDbContext _context;
 
-        public JwtService(JwtTokenUtil jwt)
+        public JwtService(JwtTokenUtil jwt, APIDbContext context)
         {
             _jwt = jwt;
+            _context = context;
         }
 
         public string? getUsername(string Autorization)
@@ -18,6 +20,20 @@ namespace quest_web_dotnet.Services
             if (AuthenticationHeaderValue.TryParse(Autorization, out var headerValue))
             {
                 return _jwt.GetUsernameFromToken(headerValue.Parameter);
+            }
+            return null;
+        }
+
+        public User? getUser(string Authorization)
+        {
+            var username = getUsername(Authorization);
+            if (username != null)
+            {
+                var user = _context.users.FirstOrDefault(u => u.Username == username);
+                if (user != null)
+                {
+                    return user;
+                }
             }
             return null;
         }
