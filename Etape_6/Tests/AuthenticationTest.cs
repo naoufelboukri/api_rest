d5553fb4fced7c0804b01c011cfe6d15f2915fe2
Naoufel <naoufelboukri@gmail.com>
2023-05-13 17:59:05 +0200

    wip

---
+++ Etape_6/Tests/AuthenticationTest.cs
@@ -0,0 +1,133 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using quest_web;
+using quest_web_dotnet.Controllers;
+using quest_web_dotnet.Models.Forms;
+using Xunit.Abstractions;
+
+namespace Test
+{
+    public class AuthenticationTest
+    {
+        private readonly ITestOutputHelper output;
+
+        public AuthenticationTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void Register()
+        {
+            var database = new DbContextOptionsBuilder<APIDbContext>()
+                            .UseInMemoryDatabase(databaseName: "quest_web")
+                            .Options;
+            var _context = new APIDbContext(database);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+
+            var _jwt = new JwtTokenUtil();
+
+            var _authenticationController = new AuthenticationController(_context, _jwt);
+            var user = new UserBody
+            {
+                Username = "test",
+                Password = "test"
+            };
+            var result = _authenticationController.register(user);
+
+            //test
+            Assert.IsType<CreatedAtActionResult>(result.Result);
+        }
+        [Fact]
+        public void RegisterDuplicata()
+        {
+            var database = new DbContextOptionsBuilder<APIDbContext>()
+                            .UseInMemoryDatabase(databaseName: "quest_web")
+                            .Options;
+            var _context = new APIDbContext(database);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+
+            var _jwt = new JwtTokenUtil();
+            var _authenticationController = new AuthenticationController(_context, _jwt);
+
+            var user = new UserBody
+            {
+                Username = "test",
+                Password = "test"
+            };
+            //first
+            _authenticationController.register(user);
+            //second
+            var second = _authenticationController.register(user);
+
+            //test
+            Assert.IsType<ConflictObjectResult>(second.Result);
+        }
+
+        [Fact]
+        public void Authenticate()
+        {
+            var database = new DbContextOptionsBuilder<APIDbContext>()
+                            .UseInMemoryDatabase(databaseName: "quest_web")
+                            .Options;
+            var _context = new APIDbContext(database);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+
+            var _jwt = new JwtTokenUtil();
+            var _authenticationController = new AuthenticationController(_context, _jwt);
+
+            var user = new UserBody
+            {
+                Username = "test",
+                Password = "test"
+            };
+
+            _authenticationController.register(user);
+            var result = _authenticationController.authenticate(user);
+
+            //test
+            Assert.IsType<OkObjectResult>(result.Result);
+
+            var obj = JsonConvert.SerializeObject(result);
+            Assert.Contains("token", obj);
+        }
+
+        [Fact]
+        public void meTest()
+        {
+            var database = new DbContextOptionsBuilder<APIDbContext>()
+                .UseInMemoryDatabase(databaseName: "quest_web")
+                .Options;
+            var _context = new APIDbContext(database);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+
+            var _jwt = new JwtTokenUtil();
+            var _authenticationController = new AuthenticationController(_context, _jwt);
+
+            var user = new UserBody
+            {
+                Username = "test",
+                Password = "test"
+            };
+
+            _authenticationController.register(user);
+            var authenticate = _authenticationController.authenticate(user);
+
+            var obj = JsonConvert.SerializeObject(authenticate);
+
+            JObject jsonToken = JObject.Parse(obj);
+            string authorization = "Bearer " + (string)jsonToken["Result"]["Value"]["token"];
+
+            var result = _authenticationController.me(authorization);
+
+            Assert.IsType<OkObjectResult>(result);
+
+        }
+    }
+}
---
+++ Etape_6/Tests/TagTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Tests
+{
+    public class TagTest
+    {
+        private readonly ITestOutputHelper output;
+        public TagTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+    }
+}
